fea6eae5d874edaa19685ccb306746971aec5240
kittipichsolg34-cpu <kittipich.sol_g34@mwit.ac.th>
2026-09-09 10:56:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const SUPABASE_URL = 'https://bcugmikcdraozmhidlls.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_dVZaVIBpHtUXfkxol22KLQ_tqCpu273';
+
+const supabaseClient =supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
+
 const userType = document.getElementById("userType");
 const rateType = document.getElementById("rateType");
 const voltage = document.getElementById("voltage");
